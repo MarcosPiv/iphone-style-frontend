00a52891b699f252b5cb388958822aa61a50a041
Marcos Pividori <mpividori3@gmail.com>
2025-03-11 22:33:27 -0300

    perf: optimize Footer component rendering, and fix bugs

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -4,10 +4,10 @@ import React from 'react'
 const Loader = () => {
   return (
     <Html>
-      <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center">
-        <div className="w-[10vw] h-[10vw] rounded-full">
+      <div className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]" role="status">
+        <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
           Loading...
-        </div>
+        </span>
       </div>
     </Html>
   )
